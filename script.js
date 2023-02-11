9e4d651d730204c5d3a97334cbe2fbca050a56ed
tusharrandhe <tushar141195@gmail.com>
2023-02-11 11:25:56 +0530

    first commit

--- script.js
+++ script.js
@@ -8,20 +8,41 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(function(value)
+  {
+    if(value.marks>50)
+    {console.log(value.marks);}
+  })
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(function(value)
+  {
+    if(value.marks>50)
+    {
+      console.log(value.marks);
+    }
+  })
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({id:4,name:"susan",age:"20",marks:45});
+  console.log(arr[3]);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr.splice(2,2);
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  arr.push({ id: 3, name: "Wick", age: "22", marks: 82 });
+  arr.push({ id: 4, name: "Alan", age: "28", marks: 88 });
+  arr.push({ id: 5, name: "Sanders", age: "39", marks: 65 });
+  console.log(arr);
 }
